2b0df9527086cb77de34760ff498a3f5998aefcf
LuppiLandia <53494649+xLamday@users.noreply.github.com>
2025-01-19 20:27:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -250,12 +250,11 @@ def main():
         text_box.send_keys(f'www.twitch.tv/{twitch_username}')
         text_box.send_keys(Keys.RETURN)
 
-        wait = WebDriverWait(driver, 240)
+        wait = WebDriverWait(driver, 5)
         wait.until(EC.presence_of_element_located((By.XPATH, "//h2[@data-a-target='stream-title']")))
         
         if set_160p == "yes":
             try:
-                time.sleep(0.5)
                 set_stream_quality(driver)
                 print(f"[{i}] Sucessfully setting the lowest quality")
             except Exception as err:
